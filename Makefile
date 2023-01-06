98d267a01db56858ae1550b3e509b09991f1af6c
Evgeniy Dammer <evgeniydammer@gmail.com>
2023-01-06 13:49:16 +0500

    Add configuration and migrations
    Add configuration and migrations

--- Makefile
+++ Makefile
@@ -4,3 +4,18 @@ run:
 
 build:
 	go build -o todo-rest-api cmd/main.go
+
+rundb:
+	docker run -d --name todo-db -e POSTGRES_PASSWORD='qwerty' -p 5432:5432 --rm postgres:9.4
+
+stopdb:
+	docker stop todo-db
+
+migrcreate:
+	migrate create -ext sql -dir ./schema -seq init
+
+migrup:
+	migrate -path ./schema -database 'postgres://postgres:qwerty@localhost:5432/postgres?sslmode=disable' up
+
+migrdown:
+	migrate -path ./schema -database 'postgres://postgres:qwerty@localhost:5432/postgres?sslmode=disable' down
--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,29 @@ import (
 	"github.com/evgeniy-dammer/todo-rest-api/pkg/handler"
 	"github.com/evgeniy-dammer/todo-rest-api/pkg/repository"
 	"github.com/evgeniy-dammer/todo-rest-api/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err)
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
